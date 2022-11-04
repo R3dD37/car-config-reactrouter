8d4d86dffebc812cff9915da8be74d122b0b15c1
R3dD37 <113446774+R3dD37@users.noreply.github.com>
2022-11-04 09:40:09 +0100

    Add files via upload

--- car_config.sql
+++ car_config.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 28 Paź 2022, 09:38
+-- Czas generowania: 04 Lis 2022, 09:38
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 7.4.29
 
@@ -41,6 +41,73 @@ CREATE TABLE `cars` (
 INSERT INTO `cars` (`id`, `model_name`, `base_price`, `img_src`) VALUES
 (1, 'Golf', '65000.00', 'https://cutt.ly/7BHYePU'),
 (2, 'ID.5', '228218.00', 'https://cutt.ly/yBHYdxZ');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `colors`
+--
+
+CREATE TABLE `colors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `hex` char(7) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `colors`
+--
+
+INSERT INTO `colors` (`id`, `name`, `hex`, `price`) VALUES
+(1, 'Srebrny \"Reflex\" Metalik', '#c2c5c9', '2300.00'),
+(2, 'Żółty \"Lime yellow\" metalik', '#ddd544', '2300.00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `colors_cars`
+--
+
+CREATE TABLE `colors_cars` (
+  `id` int(11) NOT NULL,
+  `id_car` int(11) DEFAULT NULL,
+  `id_color` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `colors_cars`
+--
+
+INSERT INTO `colors_cars` (`id`, `id_car`, `id_color`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Zastąpiona struktura widoku `default_car`
+-- (Zobacz poniżej rzeczywisty widok)
+--
+CREATE TABLE `default_car` (
+`id_car` int(11)
+,`base_price` decimal(11,2)
+,`model_name` varchar(50)
+,`img_src` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Zastąpiona struktura widoku `default_car_config`
+-- (Zobacz poniżej rzeczywisty widok)
+--
+CREATE TABLE `default_car_config` (
+`id_car` int(11)
+,`id_drive` int(11)
+,`base_price` decimal(10,2)
+,`id_color` int(11)
+);
 
 -- --------------------------------------------------------
 
@@ -139,6 +206,37 @@ INSERT INTO `gearbox` (`id`, `speedNumber`, `shortName`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `rims`
+--
+
+CREATE TABLE `rims` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `img_src` text DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura widoku `default_car`
+--
+DROP TABLE IF EXISTS `default_car`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `default_car`  AS SELECT `dcc`.`id_car` AS `id_car`, min(`dcc`.`base_price`) + (select ifnull(`colors`.`price`,0) from `colors` where `colors`.`id` = `dcc`.`id_color`) AS `base_price`, `c`.`model_name` AS `model_name`, `c`.`img_src` AS `img_src` FROM (`default_car_config` `dcc` join `cars` `c`) WHERE `dcc`.`id_car` = `c`.`id` GROUP BY `dcc`.`id_car``id_car`  ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura widoku `default_car_config`
+--
+DROP TABLE IF EXISTS `default_car_config`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `default_car_config`  AS SELECT `d`.`id_car` AS `id_car`, `d`.`id` AS `id_drive`, min(`d`.`base_price`) AS `base_price`, min(`cc`.`id_color`) AS `id_color` FROM (`drive` `d` left join `colors_cars` `cc` on(`d`.`id_car` = `cc`.`id_car`)) GROUP BY `d`.`id_car`, `d`.`id``id`  ;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura widoku `drive_select_list`
 --
 DROP TABLE IF EXISTS `drive_select_list`;
@@ -154,6 +252,20 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 ALTER TABLE `cars`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `colors_cars`
+--
+ALTER TABLE `colors_cars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_cc_car` (`id_car`),
+  ADD KEY `FK_cc_color` (`id_color`);
 
 --
 -- Indeksy dla tabeli `drive`
@@ -177,6 +289,12 @@ ALTER TABLE `gearbox`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `rims`
+--
+ALTER TABLE `rims`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT dla zrzuconych tabel
 --
 
@@ -184,6 +302,18 @@ ALTER TABLE `gearbox`
 -- AUTO_INCREMENT dla tabeli `cars`
 --
 ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT dla tabeli `colors_cars`
+--
+ALTER TABLE `colors_cars`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
@@ -205,8 +335,21 @@ ALTER TABLE `gearbox`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT dla tabeli `rims`
+--
+ALTER TABLE `rims`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Ograniczenia dla zrzutów tabel
 --
+
+--
+-- Ograniczenia dla tabeli `colors_cars`
+--
+ALTER TABLE `colors_cars`
+  ADD CONSTRAINT `FK_cc_car` FOREIGN KEY (`id_car`) REFERENCES `cars` (`id`),
+  ADD CONSTRAINT `FK_cc_color` FOREIGN KEY (`id_color`) REFERENCES `colors` (`id`);
 
 --
 -- Ograniczenia dla tabeli `drive`
